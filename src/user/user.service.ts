ee8ec05f3c9af338723a901b8ec7d0d9fe37762e
Ivan Shakhorski <ivan.shakhorski@innowise.com>
2024-11-21 14:41:17 +0100

    Feat: Add database module, Add user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
+
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.username = createUserDto.username;
+
+    return this.usersRepository.save(user);
+  }
+}
