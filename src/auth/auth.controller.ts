9b42c00da6edbcb9b977e5b6f21f469b7cb7307c
Ivan Shakhorski <ivan.shakhorski@innowise.com>
2024-11-25 17:31:49 +0100

    Feat: Add auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,15 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
-import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
   }
 }
