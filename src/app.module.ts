15a585f23546a8cbe9c01f9e3b5691c9ffea4d3b
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2024-11-30 17:04:52 +0200

    Feat: add jwt module, improve database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
+import { getDatabaseConfig } from '@config/database.config';
+import { getJwtConfig } from '@config/jwt.config';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.getOrThrow('POSTGRES_HOST'),
-        port: configService.getOrThrow('POSTGRES_PORT'),
-        database: configService.getOrThrow('POSTGRES_DB'),
-        username: configService.getOrThrow('POSTGRES_USERNAME'),
-        password: configService.getOrThrow('POSTGRES_PASSWORD'),
-        autoLoadEntities: true,
-        synchronize: process.env.NODE_ENV === 'development',
-      }),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRootAsync(getDatabaseConfig()),
+    JwtModule.registerAsync(getJwtConfig()),
     UsersModule,
     AuthModule,
   ],
