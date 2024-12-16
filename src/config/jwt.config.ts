d59162894e98d5b043f021fea9094304e30e287f
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2024-12-16 15:35:20 +0100

    Feat: Improvements for Auth module

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -5,7 +5,7 @@ export function getJwtConfig(): JwtModuleAsyncOptions {
   return {
     useFactory: (configService: ConfigService) => ({
       secret: configService.getOrThrow('JWT_SECRET'),
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: configService.getOrThrow('JWT_EXPIRE_TIME') },
     }),
     inject: [ConfigService],
   };
