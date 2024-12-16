7df279b5f7894a148013dc4713b95a0881998f53
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2024-12-16 22:48:43 +0100

    Feat: Improve AuthModule, Fix docker-compose.yaml

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Res, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -9,7 +10,16 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-up')
-  signUp(@Body() signUpDto: SignUpDto) {
-    return this.authService.signUp(signUpDto);
+  async signUp(@Body() signUpDto: SignUpDto, @Res({ passthrough: true }) response: Response) {
+    const { accessToken } = await this.authService.signUp(signUpDto);
+
+    response.cookie('Authentication', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+    });
+
+    return;
   }
 }
