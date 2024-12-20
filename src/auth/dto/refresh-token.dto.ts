91a4b3e1e49ef21649cd8d839fd36dce7b0feaa2
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2024-12-20 11:54:08 +0100

    Feat: Add refresh token request

--- src/auth/dto/refresh-token.dto.ts
+++ src/auth/dto/refresh-token.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class SignInDto {
+export class RefreshTokenDto {
   @ApiProperty({ type: String })
-  username: string;
+  refreshToken: string;
 }
