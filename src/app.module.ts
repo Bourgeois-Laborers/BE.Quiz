94fd8e5610552aa50be992176768002f7407267e
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2024-12-31 14:10:02 +0100

    Feat: Update module imports to use path aliases for improved readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UsersModule } from './users/users.module';
-import { SessionModule } from './session/session.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from '@users/users.module';
+import { SessionModule } from '@session/session.module';
+import { AuthModule } from '@auth/auth.module';
+import { EventsModule } from '@event/event.module';
 
 import { getDatabaseConfig } from '@config/database.config';
-import { EventsModule } from '@event/event.module';
 
 @Module({
   imports: [
