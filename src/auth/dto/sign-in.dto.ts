641f2b44e1f6406cefeaf3987ca61f3e8590c4e1
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2024-12-31 11:55:55 +0100

    Feat: Add UUID validation to SignInDto

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
+import { IsUUID } from 'class-validator';
 
 export class SignInDto {
   @ApiProperty({ type: String })
+  @IsUUID()
   id: string;
 }
 
