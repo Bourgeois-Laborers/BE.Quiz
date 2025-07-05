19850d33e29691140d250e11a2a1e796b9e6574b
Ivan Shakhorski <ivan.shakhorski@gmail.com>
2025-07-05 13:13:39 +0100

    Fix: Add return type to bootstrap function and invoke it correctly

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -32,4 +32,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
